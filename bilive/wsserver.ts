4ed48f3139d8ef61eeab1060a67d4a1fe6782db6
lzghzr <lzggzr@gmail.com>
2018-11-10 19:43:28 +0800

    ping

--- bilive/wsserver.ts
+++ bilive/wsserver.ts
@@ -197,9 +197,7 @@ class WSServer {
    * @memberof WSServer
    */
   private _WebSocketPing() {
-    this._wsServer.clients.forEach(client => {
-      if (client.readyState === ws.OPEN) client.ping()
-    })
+    this._wsServer.clients.forEach(client => client.ping())
   }
   /**
    * 消息广播
